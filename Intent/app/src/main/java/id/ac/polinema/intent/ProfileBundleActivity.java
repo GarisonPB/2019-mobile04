cb33d1ac209c087750f1921bb06b8983756ace00
GarisonPB <muhammadgarison99@gmail.com>
2019-09-18 16:58:46 +0700

    Bundle

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -3,8 +3,15 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileBundleActivity extends AppCompatActivity {
+
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,13 @@ public class ProfileBundleActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             // TODO: display value here
+            String username =extras.get("username").toString();
+            String name =extras.get("name").toString();
+            String age = extras.get("ageKey").toString();
+
+            usernameText.setText(username);
+            nameText.setText(name);
+            ageText.setText(age);
         }
     }
 }
